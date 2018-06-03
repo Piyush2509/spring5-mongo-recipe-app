be29309cbdf2d3599dbf289944f476d739b3dc2f
Piyush2509 <piyush.jayaswal@gmail.com>
2018-06-03 08:31:52 -0400

    Webflux exception handling

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -4,19 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import guru.springframework.commands.CategoryCommand;
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.CategoryService;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
@@ -100,20 +105,18 @@ public class RecipeController {
 		return "redirect:/";
 	}
 
-	// @ResponseStatus(HttpStatus.NOT_FOUND)
-	// @ExceptionHandler(NotFoundException.class)
-	// public ModelAndView handleNotFound(Exception exception) {
-	// log.error("Handling not found exception");
-	// log.error(exception.getMessage());
-	// ModelAndView modelAndView = new ModelAndView();
-	// modelAndView.setViewName("404error");
-	// modelAndView.addObject("exception", exception);
-	// return modelAndView;
-	// }
-
 	@ModelAttribute("categoryList")
 	public Flux<CategoryCommand> populateCategoryList() {
 		return categoryService.listAllCategories();
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler({ NotFoundException.class, TemplateInputException.class })
+	public String handleNotFound(Exception exception, Model model) {
+		log.error("Handling not found exception");
+		log.error(exception.getMessage());
+		model.addAttribute("exception", exception);
+		return "404error";
 	}
 
 }
