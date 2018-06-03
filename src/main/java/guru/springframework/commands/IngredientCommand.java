1139e5d7bd666e5b04d890e710d3f873a286e47e
Piyush2509 <piyush.jayaswal@gmail.com>
2018-06-02 21:59:41 -0400

    Ingradient  validations

--- src/main/java/guru/springframework/commands/IngredientCommand.java
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -2,6 +2,10 @@ package guru.springframework.commands;
 
 import java.math.BigDecimal;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,8 +20,12 @@ public class IngredientCommand {
 
 	private String id;
 	private String recipeId;
+	@NotBlank
 	private String description;
+	@NotNull
+	@Min(1)
 	private BigDecimal amount;
+	@NotNull
 	private UnitOfMeasureCommand uom;
 
 }
--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -2,7 +2,10 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,15 +24,24 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class IngredientController {
 
+	private static final String RECIPE_INGREDIENT_INGREDIENTFORM = "recipe/ingredient/ingredientform";
+
 	private final RecipeService recipeService;
 	private final IngredientService ingredientService;
 	private final UnitOfMeasureService unitOfMeasureService;
+
+	private WebDataBinder webDataBinder;
 
 	public IngredientController(RecipeService recipeService, IngredientService ingredientService,
 			UnitOfMeasureService unitOfMeasureService) {
 		this.recipeService = recipeService;
 		this.ingredientService = ingredientService;
 		this.unitOfMeasureService = unitOfMeasureService;
+	}
+
+	@InitBinder("ingredient")
+	public void initBinder(WebDataBinder webDataBinder) {
+		this.webDataBinder = webDataBinder;
 	}
 
 	@GetMapping("/recipe/{recipeId}/ingredients")
@@ -56,7 +68,7 @@ public class IngredientController {
 		ingredientCommand.setUom(new UnitOfMeasureCommand());
 		model.addAttribute("ingredient", ingredientCommand);
 		model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
-		return "recipe/ingredient/ingredientform";
+		return RECIPE_INGREDIENT_INGREDIENTFORM;
 	}
 
 	@GetMapping("/recipe/{recipeId}/ingredient/{id}/update")
@@ -64,11 +76,24 @@ public class IngredientController {
 		log.debug("Getting ingredient form for update for ingredient id: " + id + " for recipe id: " + recipeId);
 		model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id).block());
 		model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
-		return "recipe/ingredient/ingredientform";
+		return RECIPE_INGREDIENT_INGREDIENTFORM;
 	}
 
 	@PostMapping("/recipe/{recipeId}/ingredient")
-	public String saveRecipeIngredient(@ModelAttribute IngredientCommand command) {
+	public String saveRecipeIngredient(@ModelAttribute("ingredient") IngredientCommand command, Model model) {
+		webDataBinder.validate();
+		BindingResult bindingResult = webDataBinder.getBindingResult();
+
+		if (bindingResult.hasErrors()) {
+			bindingResult.getAllErrors().forEach(objectError -> {
+				log.debug(objectError.toString());
+			});
+
+            model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
+            
+			return RECIPE_INGREDIENT_INGREDIENTFORM;
+		}
+
 		IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command).block();
 		log.debug("Saved recipe id: " + savedCommand.getRecipeId());
 		log.debug("Saved ingredient id: " + savedCommand.getId());
