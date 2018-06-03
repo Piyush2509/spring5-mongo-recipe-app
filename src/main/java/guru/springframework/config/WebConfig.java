e18a5298887c3e46b09e68eed7fa45a8b756ac48
Piyush2509 <piyush.jayaswal@gmail.com>
2018-06-03 08:56:34 -0400

    Added a router function example

---
+++ src/main/java/guru/springframework/config/WebConfig.java
@@ -0,0 +1,27 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+
+/**
+ * Created by piyush.b.kumar on Jun 3, 2018
+ */
+@Configuration
+public class WebConfig {
+
+	@Bean
+	RouterFunction<?> routes(RecipeService recipeService) {
+		return RouterFunctions.route(GET("/api/recipes"), serverRequest -> ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON).body(recipeService.getRecipes(), Recipe.class));
+	}
+
+}
